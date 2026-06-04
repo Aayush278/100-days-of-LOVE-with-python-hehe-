61cf400445a729eac873bca680ed9dda650606d7
Aayush kumar Nayak <aayushkumarnayak27@yahoo.com>
2026-06-04 23:57:30 +0530

    Add files via upload

---
+++ Project1_Band_Name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to Band Name Generator")
+city=input("Enter your city name:\n")
+pet=input("What is the name of your pet:\n")
+print("So according to all the info your band name could be",pet+" "+city)
---
+++ hello.py
@@ -0,0 +1,11 @@
+print("hello world !") 
+# anything inside "" is a string 
+# which is a data type
+
+# string concatenation
+print("hello"+" "+"world")
+
+#variable assignment
+X="Aayush"
+print(X)
+print(X+ " is a  Good Boy")
