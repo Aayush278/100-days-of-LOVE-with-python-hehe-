661d96bbf98f69c5b2a5a5bdc79fa5552f011a1c
Aayush kumar Nayak <aayushkumarnayak27@yahoo.com>
2026-06-12 23:28:40 +0530

    Add files via upload

---
+++ Day6.py
@@ -0,0 +1,18 @@
+# Functions 
+print(len("name"))
+# here both print and len are built in functions 
+# how to make our own function:
+def my_function():
+    name=input("What is your name:\n")
+    print(f"Hello Mr.{name}")
+
+my_function()
+
+
+
+# while loop 
+number=50
+while number >5:
+    print("Hehe")
+    number-=1
+    print(number)
